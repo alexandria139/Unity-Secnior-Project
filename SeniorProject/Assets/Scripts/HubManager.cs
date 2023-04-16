85dd7b2c65c6844f53dac62344b94eaedb17075c
Levi Schoof <levischoof.99@gmail.com>
2023-04-15 21:45:08 -0500

    Add results screen to the hub

--- SeniorProject/Assets/Scripts/HubManager.cs
+++ SeniorProject/Assets/Scripts/HubManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class HubManager : MonoBehaviour
 {
     public HubWorldInteractableObject modules;
     public HubWorldInteractableObject choiceQuiz;
     public HubWorldInteractableObject carQuiz;
+
+    public GameObject CongratsScreenGameObject;
 
     public TextMeshProUGUI CurrentTaskText;
 
@@ -17,38 +20,49 @@ public class HubManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        CongratsScreenGameObject.SetActive(false);
         modules.ChangeInteractionAbility(true);
         choiceQuiz.ChangeInteractionAbility(false);
         carQuiz.ChangeInteractionAbility(false);
+        
         if (!PlayerInformation.Instance.IsModuleCheckDone())
         {
             modules.SetAsNextStep();
             CurrentTaskText.text = ModulesTask;
         }
-
+        
         else if (!PlayerInformation.Instance.IsMultChoiseDone())
         {
             choiceQuiz.ChangeInteractionAbility(true);
             choiceQuiz.SetAsNextStep();
             CurrentTaskText.text = ChoiceTask;
         }
-
-        else
+        
+        else if (!PlayerInformation.Instance.IsCarTestCompleted())
         {
             choiceQuiz.ChangeInteractionAbility(true);
             carQuiz.ChangeInteractionAbility(true);
             carQuiz.SetAsNextStep();
             CurrentTaskText.text = CarTask;
         }
-
-
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        
+        else
+        {
+            CongratsScreenGameObject.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadMainMenu()
     {
-        
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
